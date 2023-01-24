4b753debf0ee929311e01f71ce8d13f2009209ec
Victor Augusto Alves de Carvalho <105305131+VictorAlves94@users.noreply.github.com>
2023-01-24 14:10:12 -0300

    criando regras de negocio

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,35 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import com.api.parkingcontrol.repository.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+
+    }
+
+    public boolean existsByParkingSportNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSportNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment,block);
+    }
+}
